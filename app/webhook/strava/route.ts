6d4829f85a5604e6e6842a6051e4f41ba210d275
Will Gleason <wgleason9@gmail.com>
2025-09-08 20:07:11 -0700

    webhook validation

--- app/webhook/strava/route.ts
+++ app/webhook/strava/route.ts
@@ -5,6 +5,23 @@ import { NextRequest } from "next/server";
 // POST to create sub endpoint
 // https://developers.strava.com/docs/webhooks/
 
+export async function GET(req: NextRequest) {
+  const requestBody = await req.json();
+  console.log("Received Strava webhook validation:", requestBody);
+  const url = new URL(req.url);
+  const verifyToken = url.searchParams.get("hub.verify_token");
+  const hubMode = url.searchParams.get("hub.mode");
+  const hubChallenge = url.searchParams.get("hub.challenge");
+
+  if (
+    verifyToken === process.env.STRAVA_WEBHOOK_VERIFY_TOKEN &&
+    hubMode === "subscribe" &&
+    hubChallenge
+  ) {
+    return new Response(hubChallenge, { status: 200 });
+  }
+}
+
 export async function POST(req: NextRequest) {
   const requestBody = (await req.json()) as StravaWebhookEvent;
   console.log("Received Strava webhook:", requestBody);
